61893d5d37f9a6e3f76adcec2a08cebc0bc09f65
Lauren Yu <laurenkyu@gmail.com>
2020-03-09 18:44:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.random()
+
+let num3 = 9;
+let mod = num3 % 5;
+
+let max = Math.max(10, 3, 20, 18)
